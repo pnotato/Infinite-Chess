a6bde2bd342d68f850289351d8daacfd9c0437b5
Joodoon <jsm36@sfu.ca>
2024-08-20 16:06:48 -0700

    displaying traits/statuses, searching in barracks

--- src/chess/components/pieceDisplay.js
+++ src/chess/components/pieceDisplay.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './PieceDisplay.css';
 import './chessboardComponent.css';
-import { Typography } from '@mui/material';
+import { Typography, Divider, Collapse, IconButton, Chip } from '@mui/material';
 import chesspiece from '../classes/chesspiece.tsx';
 import colors from '../enums/colors.tsx';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function PieceDisplay({ piece }) {
+  const [expanded, setExpanded] = React.useState(false);
+
   if (!piece) {
     return <div className="piece-display">No piece selected</div>;
   }
@@ -36,14 +39,72 @@ function PieceDisplay({ piece }) {
     return grid;
   };
 
+  const numToTrait = (num) => {
+    switch (num) {
+      case 0:
+        return 'Ignores Blocked Attacks';
+      case 1:
+        return 'Ignores Blocked Moves';
+      case 2:
+        return 'Attacks in place';
+      case 3:
+        return 'Self Destructs';
+      case 4:
+        return 'Reflects Attacks';
+      case 5:
+        return 'Attacks in Radius';
+      case 6:
+        return 'Multiattacks';
+      case 7:
+        return 'Gives Status Effects';
+      case 8:
+        return 'Targets Allies';
+      default:
+        return 'Unknown';
+    }
+  }
+
   return (
     <div className="piece-display">
-      <Typography fontSize={"2vw"} variant="h6" align="center" sx={{color: 'white'}}>Information</Typography>
+      <Typography fontSize={"1.5vw"} variant="h6" align="center" sx={{ color: 'white' }}>Information</Typography>
+      <Divider variant='middle' sx={{ borderRadius: '5px', borderBottomWidth: 5 }} />
       <div className="display">
-        <Typography fontSize={"5vw"} align="center">
+        <Typography fontSize={"5vw"} style={{ margin: '-1vw' }} align="center">
           {rehydratedPiece.emoji}
         </Typography>
         <div className="grid-title">{rehydratedPiece.name}</div>
+        <Divider variant='middle' sx={{ borderRadius: '5px', borderBottomWidth: 5 }} />
+        <IconButton onClick={() => setExpanded(!expanded)} align='center' sx={{ color: 'black', opacity: 0.25, width: '2vw', height: '2vw' }}>
+          <ExpandMoreIcon />
+        </IconButton>
+
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <div className="traits-container">
+            <div className="traits">
+              <Typography fontSize={"1vw"} align="center" sx={{ color: 'white' }}>Traits</Typography>
+              {rehydratedPiece.traits.length > 0 ?
+                (rehydratedPiece.traits.map((trait, index) => (
+                  <Chip label={numToTrait(trait)} key={index} color='primary' sx={{ fontSize: '.75vw' }} size='small' />
+                ))) : <Typography fontSize={".75vw"} align="center" sx={{ color: 'white' }}>None</Typography>}
+            </div>
+            <div className='status-effect-display'>
+              {rehydratedPiece.statusEffects && rehydratedPiece.statusEffects.length > 0 && <Typography fontSize={"1vw"} align="center" sx={{ color: 'white' }}>Status Effects</Typography>}
+              {rehydratedPiece.statusEffects && rehydratedPiece.statusEffects.length > 0 && rehydratedPiece.statusEffects.map((effect, index) => (
+                <div>
+                  <div className='status-effect-name'>
+                    <Typography key={index} fontSize={"1vw"} align="center" sx={{ color: 'white' }}>{effect.emoji}</Typography>
+                    <Typography key={index} fontSize={".75vw"} align="center" sx={{ color: 'white' }}>{effect.name}</Typography>
+                  </div>
+                  {effect.status.map((status, index) => (
+                    <Chip label={status} key={index} color='primary' sx={{ fontSize: '.75vw' }} size='small' />
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+          <Divider variant='middle' sx={{ borderRadius: '5px', borderBottomWidth: 5 }} />
+        </Collapse>
+
         <div className={`grid-container`}>
           <div className="grid-title">Movement Pattern</div>
           <div className="grid">
--- src/chess/components/pieceGrid.js
+++ src/chess/components/pieceGrid.js
@@ -1,19 +1,32 @@
-import { Grid, Typography, Button, Box } from '@mui/material';
+import { Grid, Typography, Button, Box, Divider, TextField, InputAdornment } from '@mui/material';
 import PieceGridItem from './pieceGridItem';
 import { useState, useEffect } from 'react';
 import chesspiece from '../classes/chesspiece.tsx';
 import './chessboardComponent.css';
+import SearchIcon from '@mui/icons-material/Search';
 
 function PieceGrid({ onPieceClick, refreshGrid, setRefreshGrid }) {
-    const [pieces, setPieces] = useState([]);
-    const [filteredPieces, setFilteredPieces] = useState([]);
-    const [isFiltered, setIsFiltered] = useState(false);
+    const [allPieces, setAllPieces] = useState([]); // Stores all pieces
+    const [displayedPieces, setDisplayedPieces] = useState([]); // Pieces currently shown on the grid
+    const [showFavoritesOnly, setShowFavoritesOnly] = useState(false); // Toggle for showing only favorite pieces
+    const [searchTerm, setSearchTerm] = useState(''); // Stores the current search term
 
     const refresh = () => {
         const storedPieces = JSON.parse(localStorage.getItem('createdPieces')) || [];
         const hydratedPieces = storedPieces.map(piece => Object.assign(new chesspiece(), piece));
-        setPieces(hydratedPieces);
-        setFilteredPieces(hydratedPieces.filter(piece => piece.favourite));
+        setAllPieces(hydratedPieces);
+        filterAndSetDisplayedPieces(hydratedPieces, showFavoritesOnly, searchTerm);
+    };
+
+    const filterAndSetDisplayedPieces = (pieces, showFavoritesOnly, searchTerm) => {
+        let filtered = pieces;
+        if (showFavoritesOnly) {
+            filtered = filtered.filter(piece => piece.favourite);
+        }
+        if (searchTerm) {
+            filtered = filtered.filter(piece => piece.name.toLowerCase().includes(searchTerm.toLowerCase()));
+        }
+        setDisplayedPieces(filtered);
     };
 
     useEffect(() => {
@@ -24,20 +37,45 @@ function PieceGrid({ onPieceClick, refreshGrid, setRefreshGrid }) {
         refresh();
     }, []);
 
+    const handleSearchChange = (e) => {
+        const newSearchTerm = e.target.value.toLowerCase();
+        setSearchTerm(newSearchTerm);
+        filterAndSetDisplayedPieces(allPieces, showFavoritesOnly, newSearchTerm);
+    };
+
+    const toggleFavoriteFilter = () => {
+        const newShowFavoritesOnly = !showFavoritesOnly;
+        setShowFavoritesOnly(newShowFavoritesOnly);
+        filterAndSetDisplayedPieces(allPieces, newShowFavoritesOnly, searchTerm);
+    };
+
     return (
         <div className='piece-grid'>
-            <Typography fontSize={"2vw"} variant="h6" align="center" sx={{ color: 'white' }}>Barracks</Typography>
-            <Button onClick={() => setIsFiltered(!isFiltered)}>
-                <Typography fontSize={"1vw"} align="center" sx={{ color: 'white' }}>
-                    {isFiltered ? 'Show All' : 'Show Favorites'}
+            <Typography fontSize={"1.5vw"} variant="h6" align="center" sx={{ color: 'white' }}>Barracks</Typography>
+            <Button onClick={toggleFavoriteFilter}>
+                <Typography fontSize={".75vw"} align="center" sx={{ color: 'white' }}>
+                    {showFavoritesOnly ? 'Show All' : 'Show Favorites'}
                 </Typography>
             </Button>
+            <Divider variant='middle' sx={{ borderRadius: '5px', borderBottomWidth: 5, marginBottom: '10px' }} />
 
             <div className="piece-list">
-                {(isFiltered ? filteredPieces : pieces).map((piece, index) => (
+                {displayedPieces.map((piece, index) => (
                     <PieceGridItem key={index} piece={piece} onClick={() => onPieceClick(piece)} onRefresh={refresh} />
                 ))}
             </div>
+
+            <Divider variant='middle' sx={{ borderRadius: '5px', borderBottomWidth: 5, marginTop: '10px' }} />
+
+            <TextField variant="filled" onChange={handleSearchChange} sx={{}}
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <SearchIcon />
+                        </InputAdornment>
+                    )
+                }}
+            />
         </div>
     );
 }
